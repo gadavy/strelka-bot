47d65cf6b8ecbd39813df837a1a664db47765f30
TermiusOne <termiusone@gmail.com>
2019-04-14 19:20:48 +0300

    Fix table fk.

--- sql/tables/user_strelka.sql
+++ sql/tables/user_strelka.sql
@@ -1,6 +1,10 @@
 CREATE TABLE user_strelka (
     user_id INT NOT NULL,
     card_id INT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
     FOREIGN KEY (card_id) REFERENCES cards_strelka (id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
 )
